2e741dcbb9acb5b7dc22319a2775ec8291d348d6
quentinheredia <quentin.heredia04@gmail.com>
2025-10-30 20:12:01 -0400

    Fix SPA 404 bs

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// --- Restore SPA redirect after GitHub Pages reload ---
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+// ------------------------------------------------------
+
 createRoot(document.getElementById("root")!).render(<App />);
